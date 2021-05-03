dd56baa80b705acc018db3bb728cdf4118d99d07
Matias Brula <mbrula@itba.edu.ar>
2021-05-02 23:29:40 -0300

    all the code for exercise 2

--- src/main/java/ar/edu/itba/sds/RadiationInteraction.java
+++ src/main/java/ar/edu/itba/sds/RadiationInteraction.java
@@ -1,20 +1,23 @@
 package ar.edu.itba.sds;
 
-import ar.edu.itba.sds.algos.StepAlgorithm;
+import ar.edu.itba.sds.algos2D.StepAlgorithm;
 import ar.edu.itba.sds.objects.AlgorithmType;
 import ar.edu.itba.sds.objects.Step;
+import ar.edu.itba.sds.objects.Vector2D;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Random;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class DampedOscillation {
+public class RadiationInteraction {
     private static final String DEFAULT_CONFIG = "config.json";
     private static final String CONFIG_PARAM = "config";
 
@@ -27,14 +30,17 @@ public class DampedOscillation {
     private static final String DELTA_T_SIM_CONFIG_KEY = "delta_t_sim";
     private static final String DELTA_T_PRINT_CONFIG_KEY = "delta_t_print";
 
-    private static final String OSC_OBJECT_CONFIG_KEY = "osc";
-    private static final String OSC_ALGO_CONFIG_KEY = "algo";
-    private static final String OSC_MASS_CONFIG_KEY = "mass";
-    private static final String OSC_K_CONFIG_KEY = "k";
-    private static final String OSC_GAMMA_CONFIG_KEY = "gamma";
-    private static final String OSC_TF_CONFIG_KEY = "tf";
-    private static final String OSC_R0_CONFIG_KEY = "r0";
-    private static final String OSC_A_CONFIG_KEY = "A";
+    private static final String RAD_OBJECT_CONFIG_KEY = "rad";
+    private static final String RAD_ALGO_CONFIG_KEY = "algo";
+    private static final String RAD_MASS_CONFIG_KEY = "mass";
+    private static final String RAD_K_CONFIG_KEY = "k";
+    private static final String RAD_N_CONFIG_KEY = "N";
+    private static final String RAD_D_CONFIG_KEY = "D";
+    private static final String RAD_Q_CONFIG_KEY = "Q";
+    private static final String RAD_V0_CONFIG_KEY = "v0";
+    private static final String RAD_USE_SEED_CONFIG_KEY = "use_seed";
+    private static final String RAD_SEED_CONFIG_KEY = "seed";
+
 
     private static final double FLOAT_EPS = 1e-6;
 
@@ -42,10 +48,11 @@ public class DampedOscillation {
 
     private static String staticFilename, dynamicFilename;
     private static AlgorithmType algorithmType;
-    private static double mass, k, gamma, amp;
-    private static double r0, v0;
-    private static double time = 0.0, timeFinal;
+    private static double mass, k, d, q;
+    private static double r0;
     private static double deltaTimeSim, deltaTimePrint;
+    private static int n, v0;
+    private static long seed;
 
     public static void main(String[] args) {
         // Get simulation params
@@ -56,8 +63,17 @@ public class DampedOscillation {
             System.exit(ERROR_STATUS);
             return;
         }
-        v0 = -amp * gamma  / (2.0 * mass);
-        final BiFunction<Double, Double, Double> f = (r, v) -> -k * r - gamma * v;
+
+        // force function
+        final BiFunction<Vector2D, Map<Vector2D, Boolean>, Vector2D> f = (pi, map) ->
+                map.entrySet()
+                .stream()
+                .map(e -> Vector2D.force(pi, e.getKey(), k, q, e.getValue()))
+                .reduce(new Vector2D(), Vector2D::sum);
+        final Random r = new Random(seed);
+        final double rangeMin = (n - 1) * d / 2 - d;
+        final double rangeMax = (n - 1) * d / 2 + d;
+        r0 = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
 
         // Delete dynamicFile if already exists
         try {
@@ -76,8 +92,8 @@ public class DampedOscillation {
         long startTime = System.currentTimeMillis();
 
         // Simulation
-        final StepAlgorithm algorithm = StepAlgorithm.algorithmBuilder(algorithmType, f, deltaTimeSim, timeFinal, r0, v0, mass);
-        Step<Double> curStep = algorithm.getLastStep();
+        final StepAlgorithm algorithm = StepAlgorithm.algorithmBuilder(algorithmType, f, deltaTimeSim, r0, v0, mass, d, n);
+        Step<Vector2D> curStep = algorithm.getLastStep();
         printStep(curStep);
         while (algorithm.hasNext()) {
             curStep = algorithm.next();
@@ -91,9 +107,10 @@ public class DampedOscillation {
         System.out.printf("Simulation time \t\t ⏱  %g seconds\n", (endTime - startTime) / 1000.0);
     }
 
-    private static void printStep(Step<Double> step) {
+    private static void printStep(Step<Vector2D> step) {
         try {
-            appendToFile(dynamicFilename, String.format("%.7E\n%.7E %.7E\n*\n", step.getTime(), step.getPos(), step.getAcc()));
+            appendToFile(dynamicFilename, String.format("%.7E\n%.7E %.7E %.7E %.7E\n*\n",
+                    step.getTime(), step.getPos().getX(), step.getPos().getY(), step.getVel().getX(), step.getVel().getY()));
         } catch (IOException e) {
             System.err.println("Error writing dynamic file");
             System.exit(ERROR_STATUS);
@@ -117,16 +134,22 @@ public class DampedOscillation {
             deltaTimeSim = getConfigDouble(config, DELTA_T_SIM_CONFIG_KEY, v -> v > 0);
             deltaTimePrint = getConfigDouble(config, DELTA_T_PRINT_CONFIG_KEY, v -> v > 0 && doubleMultiple(v, deltaTimeSim));
 
-            final JSONObject oscObject = config.getJSONObject(OSC_OBJECT_CONFIG_KEY);
-            algorithmType = AlgorithmType.of(oscObject.getString(OSC_ALGO_CONFIG_KEY));
+            final JSONObject oscObject = config.getJSONObject(RAD_OBJECT_CONFIG_KEY);
+            algorithmType = AlgorithmType.of(oscObject.getString(RAD_ALGO_CONFIG_KEY));
             if (algorithmType == null) throw new ArgumentException("Invalid algorithm name");
 
-            mass = getConfigDouble(oscObject, OSC_MASS_CONFIG_KEY, v -> v > 0);
-            k = getConfigDouble(oscObject, OSC_K_CONFIG_KEY, v -> v > 0);
-            gamma = getConfigDouble(oscObject, OSC_GAMMA_CONFIG_KEY, v -> v > 0);
-            timeFinal = getConfigDouble(oscObject, OSC_TF_CONFIG_KEY, v -> v > 0 &&  doubleMultiple(v, deltaTimeSim));
-            r0 = getConfigDouble(oscObject, OSC_R0_CONFIG_KEY, v -> true);
-            amp = getConfigDouble(oscObject, OSC_A_CONFIG_KEY, v -> v > 0);
+            // get double params
+            mass = getConfigDouble(oscObject, RAD_MASS_CONFIG_KEY, v -> v > 0);
+            k = getConfigDouble(oscObject, RAD_K_CONFIG_KEY, v -> v > 0);
+            d = getConfigDouble(oscObject, RAD_D_CONFIG_KEY, v -> v > 0);
+            q = getConfigDouble(oscObject, RAD_Q_CONFIG_KEY, v -> v > 0);
+
+            // get int params
+            n = getConfigInt(oscObject, RAD_N_CONFIG_KEY, v -> v > 0);
+            v0 = getConfigInt(oscObject, RAD_V0_CONFIG_KEY, v -> v > 0);
+            final boolean useSeed = config.getBoolean(RAD_USE_SEED_CONFIG_KEY);
+            seed = (useSeed)? getConfigInt(oscObject, RAD_SEED_CONFIG_KEY, v -> v > 0) : System.nanoTime();
+
         } catch (FileNotFoundException e) {
             throw new ArgumentException(String.format("Config file %s not found", configFilename));
         } catch (IOException e) {
@@ -173,6 +196,17 @@ public class DampedOscillation {
         double value;
         try {
             value = config.getDouble(key);
+            if (!validator.test(value)) throw new NumberFormatException();
+        } catch (NumberFormatException e) {
+            throw new ArgumentException(String.format("Invalid %s number", key));
+        }
+        return value;
+    }
+
+    private static int getConfigInt(JSONObject config, String key, Predicate<Integer> validator) throws ArgumentException {
+        int value;
+        try {
+            value = config.getInt(key);
             if (!validator.test(value)) throw new NumberFormatException();
         } catch (NumberFormatException e) {
             throw new ArgumentException(String.format("Invalid %s number", key));
---
+++ src/main/java/ar/edu/itba/sds/algos2D/StepAlgorithm.java
@@ -0,0 +1,97 @@
+package ar.edu.itba.sds.algos2D;
+
+import ar.edu.itba.sds.objects.AlgorithmType;
+import ar.edu.itba.sds.objects.CustomStack;
+import ar.edu.itba.sds.objects.Step;
+import ar.edu.itba.sds.objects.Vector2D;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+import static ar.edu.itba.sds.objects.Vector2D.*;
+
+public abstract class StepAlgorithm implements Iterator<Step<Vector2D>> {
+
+    protected final CustomStack<Vector2D> pos;
+    protected final CustomStack<Vector2D> vel;
+    protected final CustomStack<Vector2D> acc;
+    protected final BiFunction<Vector2D, Map<Vector2D, Boolean>, Vector2D> f;
+    protected final Map<Vector2D, Boolean> staticParticles;
+    protected final double mass;
+    protected final double deltaT, deltaTSq;
+    protected double lastTime;
+    protected boolean done;
+    protected final double maxX, maxY;
+    protected final double dCut;
+
+    public static StepAlgorithm algorithmBuilder(AlgorithmType type, BiFunction<Vector2D, Map<Vector2D, Boolean>, Vector2D> f, double deltaT, double r0, double v0, double mass, double d, int n) {
+        switch (type) {
+            case BEEMAN:
+                return new ar.edu.itba.sds.algos2D.Beeman(f, deltaT, r0, v0, mass, d, n);
+            default:
+                throw new IllegalArgumentException("Invalid algorithm type");
+        }
+    }
+
+    protected StepAlgorithm(BiFunction<Vector2D, Map<Vector2D, Boolean>, Vector2D> f, double deltaT, double r0, double v0, double mass, double d, int n) {
+
+        this.f = f;
+        this.mass = mass;
+        this.deltaT = deltaT;
+        this.deltaTSq = deltaT * deltaT;
+        this.lastTime = 0.0;
+        this.done = false;
+        this.maxX = d * n;
+        this.maxY = d * (n - 1);
+        this.dCut = 0.01 * d;
+
+        this.staticParticles = initStaticParticles(d, n);
+        final Vector2D pos0 = new Vector2D(0.0, r0);
+        final Vector2D vel0 = new Vector2D(v0, 0.0);
+        final Vector2D acc0 = scalar(f.apply(pos0, this.staticParticles), 1 / this.mass);
+
+        final Step<Vector2D> prevStep = eulerPrecedingStep(this.lastTime, pos0, vel0, acc0);
+
+        this.pos = new CustomStack<>();
+        pos.push(prevStep.getPos());
+        pos.push(pos0);
+        this.vel = new CustomStack<>();
+        pos.push(prevStep.getVel());
+        pos.push(vel0);
+        this.acc = new CustomStack<>();
+        pos.push(prevStep.getAcc());
+        pos.push(acc0);
+    }
+
+    public Step<Vector2D> getLastStep() {
+        return new Step<>(lastTime, pos.peek(), vel.peek(), acc.peek());
+    }
+
+    // TODO check if min X is ok
+    @Override
+    public boolean hasNext() {
+        if (lastTime < deltaT) return true;
+        if (pos.peek().getX() >= maxX || pos.peek().getX() <= 0.0 || pos.peek().getY() >= maxY || pos.peek().getY() <= 0.0) return false;
+        return staticParticles.keySet().stream().noneMatch(v -> mod(pos.peek(), v) < dCut);
+    }
+
+    // private auxiliary functions
+
+    private Step<Vector2D> eulerPrecedingStep(double t, Vector2D r, Vector2D v, Vector2D a) {
+        final Vector2D prevPos = sum(sub(r, scalar(v, deltaT)), scalar(a, deltaTSq / (2.0 * mass)));
+        final Vector2D prevVel = sub(v, scalar(inv(a), deltaT / mass));
+        final Vector2D prevAcc = scalar(f.apply(prevPos, staticParticles), 1 / mass);
+        return new Step<>(t - deltaT, prevPos, prevVel, prevAcc);
+    }
+
+    private Map<Vector2D, Boolean> initStaticParticles(double d, int n) {
+        final Map<Vector2D, Boolean> map = new HashMap<>(n * n);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                map.put(new Vector2D((i + 1) * d, j * d), (i + j) % 2 == 0);
+
+        return map;
+    }
+}
