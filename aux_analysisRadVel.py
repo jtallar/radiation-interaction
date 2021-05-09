5860b1bdbb1165056616e8371ba94af702a81b22
Julian Tallar <jtallar@itba.edu.ar>
2021-05-08 22:25:21 -0300

    Final values for aux_analysis

--- aux_analysisRadVel.py
+++ aux_analysisRadVel.py
@@ -10,18 +10,18 @@ import utils
 # Values obtained by runnning
 #   ./multipleV0.sh 10000 3
 # Replace with repetition count
-rep_count = 3
+rep_count = 20
 # Replace delta_t values here (delta_t_sim = delta_t_print)
-v0s = [10000.0, 20000.0, 30000.0, 40000.0, 50000.0, 60000.0, 70000.0, 80000.0, 90000.0, 100000.0]
+v0s = [10000.0, 12500.0, 15000.0, 17500.0, 20000.0, 22500.0, 25000.0, 27500.0, 30000.0, 32500.0, 35000.0, 37500.0, 40000.0, 42500.0, 45000.0, 47500.0, 50000.0, 52500.0, 55000.0, 57500.0, 60000.0, 62500.0, 65000.0, 67500.0, 70000.0, 72500.0, 75000.0, 77500.0, 80000.0, 82500.0, 85000.0, 87500.0, 90000.0, 92500.0, 95000.0, 97500.0, 100000.0]
 # Replace l_tot values here
-l_tot_mean = [8e-08, 1.5e-07, 1.602e-07, 1.5e-07, 1.6002e-07, 1.6004e-07, 1.60012e-07, 1.60007e-07, 1.6007e-07, 1.60005e-07]
-l_tot_std = [5e-08, 2e-08, 1e-10, 3e-08, 2e-12, 3e-11, 2e-12, 4e-12, 8e-11, 4e-12]
+l_tot_mean = [7.949E-08, 9.821E-08, 9.845E-08, 1.313E-07, 1.361E-07, 1.443E-07, 1.596E-07, 1.496E-07, 1.535E-07, 1.553E-07, 1.559E-07, 1.555E-07, 1.561E-07, 1.397E-07, 1.617E-07, 1.600E-07, 1.622E-07, 1.556E-07, 1.603E-07, 1.542E-07, 1.375E-07, 1.600E-07, 1.546E-07, 1.556E-07, 1.600E-07, 1.582E-07, 1.526E-07, 1.525E-07, 1.604E-07, 1.604E-07, 1.525E-07, 1.602E-07, 1.592E-07, 1.525E-07, 1.411E-07, 1.526E-07, 1.601E-07]
+l_tot_std = [6.288E-08, 6.153E-08, 5.948E-08, 6.060E-08, 4.324E-08, 6.601E-08, 3.896E-08, 6.508E-08, 5.318E-08, 2.299E-08, 1.523E-08, 2.748E-08, 1.925E-08, 5.196E-08, 4.279E-09, 7.762E-09, 5.290E-09, 2.261E-08, 7.284E-10, 2.650E-08, 5.498E-08, 1.684E-11, 3.208E-08, 1.494E-08, 4.698E-11, 7.995E-09, 3.358E-08, 3.358E-08, 1.506E-09, 8.431E-10, 3.356E-08, 7.146E-10, 3.716E-09, 3.356E-08, 4.753E-08, 3.358E-08, 2.797E-10]
 # Replace ending values here
-top_wall_count = [x / rep_count for x in [0, 1, 0, 1, 0, 0, 0, 0, 0, 0]]
-right_wall_count = [x / rep_count for x in [0, 1, 3, 2, 3, 3, 3, 3, 3, 3]]
-bot_wall_count = [x / rep_count for x in [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]
-left_wall_count = [x / rep_count for x in [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-collision_count = [x / rep_count for x in [2, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+top_wall_count = [x / rep_count for x in [0, 1, 1, 1, 4, 3, 3, 1, 4, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+right_wall_count = [x / rep_count for x in [0, 0, 1, 6, 12, 7, 14, 13, 14, 18, 17, 18, 18, 17, 20, 19, 18, 19, 20, 19, 17, 20, 19, 18, 20, 19, 19, 19, 20, 20, 19, 20, 19, 19, 17, 19, 20]]
+bot_wall_count = [x / rep_count for x in [1, 2, 2, 5, 0, 4, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+left_wall_count = [x / rep_count for x in [11, 8, 4, 2, 2, 4, 1, 3, 2, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+collision_count = [x / rep_count for x in [8, 9, 12, 6, 2, 2, 1, 2, 0, 1, 1, 0, 1, 2, 0, 1, 1, 1, 0, 1, 3, 0, 1, 2, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 3, 1, 0]]
 
 # Initialize plotting
 utils.init_plotter()
@@ -33,7 +33,7 @@ utils.plot_multiple_values(
     sci_x=True, sci_y=False, min_val_y=-0.1, max_val_y=1.1)
 
 # Plot errorbars for trajectory length = f(v0)
-utils.plot_error_bars(v0s, 'velocidad inicial (m/s)', l_tot_mean, 'longitud de trayectoria (m)', l_tot_std, 0, 1, sci_x=True)
+utils.plot_error_bars(v0s, 'velocidad inicial (m/s)', l_tot_mean, 'longitud de trayectoria (m)', l_tot_std, 0, 2, sci_x=True)
 
 # Hold execution
 utils.hold_execution()
